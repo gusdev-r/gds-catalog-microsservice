9e02384b60ece27e9a43f007e7bacbae745da65d
Gustavo Moreira <g.hen.moreira@gmail.com>
2025-03-19 22:51:05 -0300

    feat: entity implementation in Category Entity

--- src/category/domain/entities.py
+++ src/category/domain/entities.py
@@ -1,13 +1,22 @@
 from datetime import datetime
 from dataclasses import dataclass, field
 from typing import Optional
-import uuid
+from __seedwork.domain.entities import Entity
 
 
-@dataclass(kw_only=True, frozen=True)
-class Category:
-    id: uuid.UUID = field(default_factory=lambda: uuid.uuid4)
+@dataclass(kw_only=True, frozen=True, slots=True)
+class Category(Entity):
     name: str
     description: Optional[str] = None
     is_active: Optional[bool] = True
     created_at: Optional[datetime] = field(default_factory=lambda: datetime.now())
+
+    def update(self, name: str, description: str):
+        self._set("name", name)
+        self._set("description", description)
+
+    def activate(self):
+        self._set("is_active", True)
+
+    def deactivate(self):
+        self._set("is_active", False)
