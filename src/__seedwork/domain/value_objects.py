23aca5ebe4f84030c43d88671213040a89a4efee
Gustavo Moreira <g.hen.moreira@gmail.com>
2025-03-19 22:51:21 -0300

    feat: value object abstraction class

--- src/__seedwork/domain/value_objects.py
+++ src/__seedwork/domain/value_objects.py
@@ -1,6 +1,20 @@
-from dataclasses import dataclass, field
+from abc import ABC
+from dataclasses import dataclass, field, fields
+import json
 import uuid
 from __seedwork.domain.exceptions import InvalidUuidException
+
+
+@dataclass(frozen=True)
+class ValueObject(ABC):
+    def __str__(self) -> str:
+        fields_name = [field.name for field in fields(self)]
+        if len(fields_name) == 1:
+            return str(getattr(self, fields_name[0]))
+        else:
+            json.dumps(
+                {field_name: getattr(self, field_name) for field_name in fields_name}
+            )
 
 
 @dataclass(frozen=True)
