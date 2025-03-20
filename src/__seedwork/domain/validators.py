50feedca34bd78412a7d9d15d7eae59b4441ffa1
Gustavo Moreira <g.hen.moreira@gmail.com>
2025-03-19 22:47:56 -0300

    feat: base validation for entity implementations

---
+++ src/__seedwork/domain/validators.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass
+from typing import Any
+
+from .exceptions import ValidationException
+
+
+@dataclass(frozen=True, slots=True)
+class ValidatorRules:
+    value: Any
+    prop: str
+
+    @staticmethod
+    def values(value: Any, prop: str):
+        return ValidatorRules(value, prop)
+
+    def required(self) -> "ValidationException":
+        if self.value is None or self.value == "":
+            raise ValidationException()
+        return self
+
+    def string(self) -> "ValidationException":
+        if not isinstance(self.value, str):
+            raise ValidationException()
+        return self
+
+    def max_length(self, max_length) -> "ValidationException":
+        if len(self.value) > max_length:
+            raise ValidationException()
+        return self
+
+    def boolean(self) -> "ValidationException":
+        if self.value is not True and self.value is not False:
+            raise ValidationException()
+        return self
