cfd0ba57322de87a6540c78c513f2f2ab3239bf3
Gustavo Moreira <g.hen.moreira@gmail.com>
2025-03-21 23:27:16 -0300

    feat: validation exception seedwork

--- src/__seedwork/domain/exceptions.py
+++ src/__seedwork/domain/exceptions.py
@@ -1,3 +1,8 @@
 class InvalidUuidException(Exception):
     def __init__(self, error="Id must be a valid UUID") -> None:
         super().__init__(error)
+
+
+class ValidationException(Exception):
+    def __init__(self, error="A validation error occurs") -> None:
+        super().__init__(error)
