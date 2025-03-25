07f5dd8b79ca0afa70325675b8f1eea58179b226
Gustavo Moreira <g.hen.moreira@gmail.com>
2025-03-24 23:03:49 -0300

    feat: implementing validator rules in category entity

--- src/category/domain/entities.py
+++ src/category/domain/entities.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from dataclasses import dataclass, field
 from typing import Optional
 from __seedwork.domain.entities import Entity
+from __seedwork.domain.validators import ValidatorRules
 
 
 @dataclass(kw_only=True, frozen=True, slots=True)
@@ -10,6 +11,14 @@ class Category(Entity):
     description: Optional[str] = None
     is_active: Optional[bool] = True
     created_at: Optional[datetime] = field(default_factory=lambda: datetime.now())
+
+    def __new__(cls, **kwargs):
+        cls.validate(
+            name=kwargs.get("name"),
+            description=kwargs.get("description"),
+            is_active=kwargs.get("is_active"),
+        )
+        return super(Category, cls).__new__(cls)
 
     def update(self, name: str, description: str):
         self._set("name", name)
@@ -20,3 +29,9 @@ class Category(Entity):
 
     def deactivate(self):
         self._set("is_active", False)
+
+    @classmethod
+    def validate(cls, name: str, description: str, is_active: bool = None):
+        ValidatorRules.values(value=name, prop="name").required().max_length(255)
+        ValidatorRules.values(value=description, prop="description").string()
+        ValidatorRules.values(value=is_active, prop="is_active").boolean()
